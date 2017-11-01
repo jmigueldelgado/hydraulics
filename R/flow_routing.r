d9ed9da9a5a8cb4e6e4c2ca94761d95130c7675f
Jose Delgado <jmigueldelgado@gmail.com>
2017-11-01 15:37:24 +0100

    adapts flow routing in catchment to new paradigm of data frames

--- R/flow_routing.r
+++ R/flow_routing.r
@@ -16,35 +16,23 @@ kinematic_wave_celerity_constant_B <- function(B,dQdy) ### page 284 chow
 
 
 #' Muskingum's effluent in pipe or catchment reach as in the documentation of citydrain 2
-#' @param Qin is a SpatialPointsDataFrame inflows to reach in step i
-#' @param V is a SpatialPointsDataFrame of volumes in reach in step i-1
-#' @param Qout is a SpatialPointsDataFrame of outflows from reach in step i
-#' @param parm.df is SpatialPointsDataFrame pointing to the centroids of the catchment and containing dt, K, X and the id of the upstream subbasin
-#' @param i is the time step
+#' @param state is a data frame of colums: Qin column of inflows to reach in step i; Vprevious is the column of volumes in reach or basin in step i-1; Qout is the column of outflows from reach in step i; dt, K, X are timestep, K and X as in muskingum
 #' @export
-Q_out_muskingum <- function(Qin,V,parm.df,i)
+Q_out_muskingum <- function(state)
 {
-    dt <- parm.df@data$dt
-    K <- parm.df@data$K
-    X <- parm.df@data$X
 
-    Cx <- (dt/2-K*X)/(dt/2+K*(1-X))
-    Cy <- 1/(dt/2+K*(1-X))
-    Qout <- Cx*Qin+Cy*V
-    return(Qout)
+    out <- mutate(state,Cx=(dt/2-K*X)/(dt/2+K*(1-X)),Cy=1/(dt/2+K*(1-X)),out=Cx*Qin+Cy*Vprevious)
+
+    return(out)
 }
 
 #' Stored volume in step i. from the documentation of citydrain 2
-#' @param Qin is a SpatialPointsDataFrame inflows to reach in step i
-#' @param V is a SpatialPointsDataFrame of volumes in reach in step i-1
-#' @param Qout is a SpatialPointsDataFrame of outflows from reach in step i
-#' @param parm.df is SpatialPointsDataFrame pointing to the centroids of the catchment and containing dt, K, X and the id of the upstream subbasin
-#' @param i is the time step
+#' @param state is a data frame of colums: Qin column of inflows to reach in step i; Vprevious is the column of volumes in reach or basin in step i-1; Qout is the column of outflows from reach in step i; dt, K, X are timestep, K and X as in muskingum
 #' @export
-V_muskingum <- function(Qin,Qe,V,parm.df)
+V_muskingum <- function(state)
 { 
-    Vpresent <- (Qin-Qout)*parm.df$dt[1]+V
-    return(Vpresent)
+    out <- mutate(state,V=(Qin-Qout)*dt[1]+Vprevious)
+    return(out)
 }
 
 
